1eb9faf2ac671f89ad2ceefc8984ccb227fa057a
idan <idansagi@gmail.com>
2025-11-11 09:30:31 +0000

    fix url

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,23 @@ import * as path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-  plugins: [react()],
-  publicDir: 'public',
-  server: {
-    host: true,
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+
+export default defineConfig(({ command }) => {
+  return {
+    build: {
+      sourcemap: true,
     },
-  },
+    base: command === 'serve' ? '/' : '/movie-app/',
+    plugins: [react()],
+    publicDir: 'public',
+    server: {
+      host: true,
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 })
